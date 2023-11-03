846c0b0a2a45d4e7290a2f60472ca74e49a000b8
lawrence-254 <lodhiambo26@gmail.com>
2023-11-03 07:28:05 +0000

    cash only cache id update

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -25,6 +25,7 @@ def hbnb():
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
+    cache_id = str.uuid(uuid4())
 
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
@@ -38,7 +39,8 @@ def hbnb():
     return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
