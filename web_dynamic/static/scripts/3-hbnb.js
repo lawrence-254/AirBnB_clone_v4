4d44ca9fe71e06f4a4818a664c77211c819dd96c
lawrence-254 <lodhiambo26@gmail.com>
2023-11-03 11:27:17 +0000

    Fetch places complete

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -25,3 +25,31 @@ $.ajax({
   dataType: 'json',
   contentType: 'application/json',
   success: function (data) {
+    let pDict;
+    data.sort((first, second) => first.name.localeCompare(second.name));
+    let placesSection = $('SECTION.places');
+    data.forEach((pDict) => {
+      let place = $('<article></article>');
+
+      let title = $('<div class="title"></div>');
+      title.append($('<h2></h2>').text(pDict.name));
+      title.append($('<div class="price_by_night"></div>').text(pDict.price_by_night));
+
+      let info = $('<div class="information"></div>');
+      let guests = $('<div class="max_guest"></div>');
+      guests.append(pDict.max_guest + ' Guests');
+
+      let rooms = $('<div class="number_rooms"></div>');
+      rooms.append(pDict.number_rooms + ' Bedrooms');
+
+      let bathrooms = $('<div class="number_bathrooms">');
+      bathrooms.append(pDict.number_bathrooms + ' Bathroom');
+
+      let desc = $('<div class="description"></div>').text(pDict.description);
+
+      info.append(guests, rooms, bathrooms);
+      place.append(title, info, desc);
+      placesSection.append(place)
+     });
+   }
+ });
